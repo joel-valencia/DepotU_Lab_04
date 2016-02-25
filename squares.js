233d997cd8adbb48ba3e852089572d1199686e70
Joel Valencia <trekkerj1@gmail.com>
2016-02-25 14:25:52 -0600

    add square container and even/odd logic

--- squares.js
+++ squares.js
@@ -4,25 +4,45 @@ document.addEventListener('DOMContentLoaded', function() {
     button.id = 'my-button';
     document.body.appendChild(button)
     button.onclick = createSquare;
+    
+    var container = document.createElement('div');
+    container.id = 'container';
+    document.body.appendChild(container);
 });
 
 function randomNum() {
     return Math.floor((Math.random() * 150)) + 50;
 }
 
+var numSquares = 0;
+
 function createSquare() {
     var square = document.createElement('div');
     square.className = 'square';
     square.id = document.getElementsByClassName('square').length;
-    document.body.appendChild(square);
+    document.getElementById('container').appendChild(square);
     
     square.onmouseover = function() {
-        square.textContent = square.id;
+        this.textContent = square.id;
     };
     square.onmouseout = function() {
-        square.textContent = "";
+        this.textContent = "";
     };
     square.onclick = function() {
-        square.style.backgroundColor = 'rgb(' + randomNum() + ',' + randomNum() + ',' + randomNum() + ')';
+        this.style.backgroundColor = 'rgb(' + randomNum() + ',' + randomNum() + ',' + randomNum() + ')';
+    };
+    square.ondblclick = function() {
+
+        if(this.id % 2 == 0) {
+            var nextSquare = this.nextSibling;
+            if (nextSquare) {
+                nextSquare.parentNode.removeChild(nextSquare);
+            };
+            } else {
+            var prevSquare = this.previousSibling;
+            if (prevSquare) {
+                prevSquare.parentNode.removeChild(prevSquare);
+            };
+        }
     };
 }
